8de851f4c605742ab3d66b059d96bb67ae0c5589
PeterJorgensen <dasbomber.jorgensen@gmail.com>
2021-11-16 18:26:26 +0100

    Made Student class as an Entity and implemented CRUD-methods in StudentService

--- src/main/java/se/iths/entity/Student.java
+++ src/main/java/se/iths/entity/Student.java
@@ -1,12 +1,14 @@
 package se.iths.entity;
 
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+@Entity
 public class Student {
 
     @Id
--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -7,14 +7,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
-@Transactional
-public class StudentRest {
-    @PersistenceContext
-    EntityManager entityManager;
 
-    public Student createStudent(Student student) {
-        entityManager.persist(student);
-        return student;
-    }
+public class StudentRest {
+
 
 }
