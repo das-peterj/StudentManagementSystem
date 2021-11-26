2c8769f049dd73c4bf53ade874113e7602ac71e3
PeterJorgensen <dasbomber.jorgensen@gmail.com>
2021-11-26 02:44:50 +0100

    Added service classes for teacher&subject

--- src/main/java/se/iths/service/SubjectService.java
+++ src/main/java/se/iths/service/SubjectService.java
@@ -1,6 +1,10 @@
 package se.iths.service;
 
 
+import se.iths.entity.Student;
+import se.iths.entity.Subject;
+import se.iths.entity.Teacher;
+
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -8,6 +12,7 @@ import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Transactional
 @Consumes(MediaType.APPLICATION_JSON)
@@ -22,6 +27,70 @@ public class SubjectService {
     @Inject
     TeacherService teacherService;
 
+    public Subject createSubject(Subject subject) {
+        entityManager.persist(subject);
+        return subject;
+    }
 
+    public Subject updateSubject(Long id, String subject) {
+        Subject foundSubject = entityManager.find(Subject.class, id);
 
+        foundSubject.setSubject(subject);
+        return entityManager.merge(foundSubject);
+    }
+
+    public Subject findSubjectById(Long id) {
+        return entityManager.find(Subject.class, id);
+    }
+
+    public List<Subject> getAllSubjects() {
+        return entityManager.createQuery(
+                "SELECT s FROM Subject s", Subject.class).getResultList();
+    }
+
+    public void deleteSubject(Long id) {
+        Subject foundSubject = entityManager.find(Subject.class, id);
+        entityManager.remove(foundSubject);
+    }
+
+    public Subject addTeacherToSubject(Long id, Long teacherId) {
+        Subject foundSubject = findSubjectById(id);
+        Teacher foundTeacher = teacherService.findTeacherById(teacherId);
+
+        foundSubject.setTeacher(foundTeacher);
+        foundTeacher.addSubject(foundSubject);
+
+        return foundSubject;
+    }
+
+    public Subject deleteTeacherFromSubject(Long id, Long teacherId) {
+        Subject foundSubject = findSubjectById(id);
+        Teacher foundTeacher = teacherService.findTeacherById(teacherId);
+
+        foundSubject.setTeacher(null);
+        foundTeacher.deleteSubject(foundSubject);
+        entityManager.persist(foundSubject);
+
+        return foundSubject;
+    }
+
+    public Subject addStudentToSubject(Long id, Long studentId) {
+        Subject foundSubject = findSubjectById(id);
+        Student foundStudent = studentService.findStudentById(studentId);
+
+        foundSubject.addStudent(foundStudent);
+        entityManager.persist(foundSubject);
+
+        return foundSubject;
+    }
+
+    public Subject deleteStudentFromSubject(Long id, Long studentId) {
+        Subject foundSubject = findSubjectById(id);
+        Student foundStudent = studentService.findStudentById(studentId);
+
+        foundSubject.deleteStudent(foundStudent);
+        entityManager.persist(foundSubject);
+
+        return foundSubject;
+    }
 }
--- src/main/java/se/iths/service/TeacherService.java
+++ src/main/java/se/iths/service/TeacherService.java
@@ -1,4 +1,54 @@
 package se.iths.service;
 
+import se.iths.entity.Teacher;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Transactional
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class TeacherService {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public Teacher createTeacher(Teacher teacher) {
+        entityManager.persist(teacher);
+        return teacher;
+    }
+
+    public Teacher updateTeacher(Long id, Teacher teacher) {
+        Teacher foundTeacher = entityManager.find(Teacher.class, id);
+
+        foundTeacher.setFirstName(teacher.getFirstName());
+        foundTeacher.setLastName(teacher.getLastName());
+        foundTeacher.setEmail(teacher.getEmail());
+        foundTeacher.setPhoneNumber(teacher.getPhoneNumber());
+
+        return entityManager.merge(foundTeacher);
+    }
+
+    public Teacher findTeacherById(Long id) {
+        return entityManager.find(Teacher.class, id);
+    }
+
+    public List<Teacher> getAllTeachers() {
+        return entityManager.createQuery(
+                "SELECT t from Teacher t", Teacher.class) .getResultList();
+
+    }
+
+    public void deleteTeacher(Long id) {
+        Teacher foundTeacher = entityManager.find(Teacher.class, id);
+        entityManager.remove(foundTeacher);
+    }
+
+
 }
